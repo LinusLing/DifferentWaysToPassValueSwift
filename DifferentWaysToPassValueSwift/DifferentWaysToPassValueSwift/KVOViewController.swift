35c5458872716ca02843b368e43a0ac093feecf0
Linus <360725966@qq.com>
2015-09-02 15:05:03 +0800

    add KVO

---
+++ DifferentWaysToPassValueSwift/DifferentWaysToPassValueSwift/KVOViewController.swift
@@ -0,0 +1,25 @@
+//
+//  KVOViewController.swift
+//  DifferentWaysToPassValueSwift
+//
+//  Created by Linus on 15-9-2.
+//  Copyright (c) 2015年 Linus. All rights reserved.
+//
+
+import UIKit
+
+class KVOViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
