5529bdb2a4c161fc37230661d42fc97ff6fb6297
Linus <360725966@qq.com>
2015-09-07 21:39:24 +0800

    add NotificationViewController

---
+++ DifferentWaysToPassValueSwift/DifferentWaysToPassValueSwift/NotificationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NotificationViewController.swift
+//  DifferentWaysToPassValueSwift
+//
+//  Created by Linus on 15-9-7.
+//  Copyright (c) 2015年 Linus. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
